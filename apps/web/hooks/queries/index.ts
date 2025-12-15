24d7b03c8ece6d116ee1e24527efc454d4cc79ad
arazik-code <arazik-code@users.noreply.github.com>
2025-12-15 22:39:22 +0100

    chore(web/hooks): dedupe exports in hooks/queries/index.ts

--- apps/web/hooks/queries/index.ts
+++ apps/web/hooks/queries/index.ts
@@ -4,11 +4,8 @@
  */
 
 export * from './use-projects';
-export * from './use-kpi';export * from './use-carbon';
-export * from './use-documents';
-export * from './use-governance';
-export * from './use-lp';
-export * from './use-notifications';export * from './use-carbon';
+export * from './use-kpi';
+export * from './use-carbon';
 export * from './use-documents';
 export * from './use-governance';
 export * from './use-lp';
